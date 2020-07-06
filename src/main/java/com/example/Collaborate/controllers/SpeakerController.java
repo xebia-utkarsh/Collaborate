b3836f77026c7806ee7ac72f1f20078d53dc1b8e
Utkarsh Singh <Utkarsh.Singh@XEBIAINDIA.LOCAL>
2020-07-06 17:07:31 +0530

    a

---
+++ src/main/java/com/example/Collaborate/controllers/SpeakerController.java
@@ -0,0 +1,45 @@
+package com.example.Collaborate.controllers;
+
+import com.example.Collaborate.models.Session;
+import com.example.Collaborate.models.Speaker;
+import com.example.Collaborate.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakerController {
+    @Autowired
+    public SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list(){
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        Speaker existingSpeaker=speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker,existingSpeaker,"speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+}
---
+++ src/test/java/com/example/Collaborate/CollaborateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Collaborate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
